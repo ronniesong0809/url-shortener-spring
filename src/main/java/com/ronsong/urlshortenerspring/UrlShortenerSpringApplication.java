3099f9759206208d69d8751700d04dd4446c7fd9
Ronnie Song <ronniesong0809@gmail.com>
2022-02-24 23:15:00 -0800

    init

---
+++ src/main/java/com/ronsong/urlshortenerspring/UrlShortenerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ronsong.urlshortenerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronsong/urlshortenerspring/UrlShortenerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronsong.urlshortenerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
